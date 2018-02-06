4e9ff7d98407cbb56c3b314194655ffb4634f444
Ankush Kushwaha <ankush.kushwaha@starcut.com>
2018-02-06 15:46:34 +0200

    Improve UI for iPhone X
    - Hide status bas in all view-controllers
    - Fix top bar was clipped from the top in
    tutorial-viewController

--- SmartDiaper/SDAppDelegate.swift
+++ SmartDiaper/SDAppDelegate.swift
@@ -15,7 +15,7 @@ class SDAppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        UIApplication.shared.isStatusBarHidden = true
         return true
     }
 
---
+++ SmartDiaper/View/ViewControllers/SDTutorialViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SDTutorialViewController.swift
+//  SmartDiaper
+//
+//  Created by Ankush Kushwaha on 2/6/18.
+//  Copyright © 2018 Starcut. All rights reserved.
+//
+
+import UIKit
+
+class SDTutorialViewController: UIViewController {
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+}
