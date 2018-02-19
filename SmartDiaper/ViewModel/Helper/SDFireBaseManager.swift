55579a4d1dca53368702cd31ad6cae1f9edc725a
Ankush Kushwaha <ankush.kushwaha@starcut.com>
2018-02-19 16:01:31 +0200

    Replace hard-coded keys to veriables in SDFireBaseManager

--- SmartDiaper/ViewModel/Helper/SDFireBaseManager.swift
+++ SmartDiaper/ViewModel/Helper/SDFireBaseManager.swift
@@ -12,10 +12,19 @@ import FirebaseDatabase
 
 class SDFireBaseManager {
 
+    struct FirebaseKeys {
+        static let staging = "staging"
+        static let production = "production"
+        static let specificGravity = "specific_gravity"
+        static let phKey = "ph"
+        static let deviceId = "device_id"
+        static let timestamp = "timestamp"
+    }
+
     #if DEBUG
-        private let configuration = "staging"
+    private let configuration = FirebaseKeys.staging
     #else
-        private let configuration = "production"
+    private let configuration = FirebaseKeys.production
     #endif
 
     static let sharedInstance = SDFireBaseManager()
@@ -35,10 +44,10 @@ class SDFireBaseManager {
         let deviceName = UIDevice.current.name
         let uniqueId = deviceName + "__" + deviceId
 
-        let post = ["timestamp": timeStamp,
-                    "specificGravity": specificGravity,
-                    "ph": phValue,
-                    "deviceId": uniqueId] as [String: Any]
+        let post = [FirebaseKeys.timestamp: timeStamp,
+                    FirebaseKeys.specificGravity: specificGravity,
+                    FirebaseKeys.phKey: phValue,
+                    FirebaseKeys.deviceId: uniqueId] as [String: Any]
 
         let locationRef = rootDB.childByAutoId()
         locationRef.setValue(post)
@@ -49,20 +58,21 @@ class SDFireBaseManager {
 
         let fileName = "data"
         let path = NSURL(fileURLWithPath: "/Users/startcut/Desktop").appendingPathComponent(fileName)
-        var csvText = "TimeStamp,ph, SpecificGravity,deviceId\n"
+        var csvText =  "\(FirebaseKeys.timestamp), \(FirebaseKeys.phKey)"
+        csvText = "\(csvText), \(FirebaseKeys.specificGravity), \(FirebaseKeys.deviceId))\n"
 
         rootDB.observe(DataEventType.value, with: { (snapshot) in
             let postDict = snapshot.value as? [String: AnyObject] ?? [:]
 
             for (_, value) in postDict {
-//                print("\(key) = \(value)")
+                //                print("\(key) = \(value)")
 
                 let dataEntry = value as? [String: AnyObject]
 
-                let ph = String(format: "%d", dataEntry!["ph"] as? Int ?? 0)
-                let sg = String(format: "%f", dataEntry!["specificGravity"] as? Double ?? 0.0)
-                let deviceId = dataEntry!["deviceId"] as? String ?? ""
-                let timeStamp = dataEntry!["timestamp"] as? String ?? ""
+                let ph = String(format: "%d", dataEntry![FirebaseKeys.phKey] as? Int ?? 0)
+                let sg = String(format: "%f", dataEntry![FirebaseKeys.specificGravity] as? Double ?? 0.0)
+                let deviceId = dataEntry![FirebaseKeys.deviceId] as? String ?? ""
+                let timeStamp = dataEntry![FirebaseKeys.timestamp] as? String ?? ""
 
                 let newLine = "\(timeStamp), \(ph), \(sg), \(deviceId)\n"
                 csvText.append(newLine)
@@ -77,9 +87,7 @@ class SDFireBaseManager {
                 }
                 print(path ?? "not found")
             }
-
         })
-
     }
 
 }
